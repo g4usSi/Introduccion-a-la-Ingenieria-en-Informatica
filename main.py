4f1f68ca4b7a36259cd30aec1aee1e9129d07de3
g4usSi <geovanny.alcon2@gmail.com>
2024-05-08 09:10:09 -0600

    V Serie
    Agregue prints que muestran los datos de la universidad

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print("Justo Geovanny Alcon Mendoza - 1578324")
+print("Universidad Rafel Landivar")
+print("Facultad de Ingenieria")
+print("Primer Semestre - Ingenieria en Informatica y Sistemas")
